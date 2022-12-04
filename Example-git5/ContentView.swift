665f32f0a5aad832873b539f30f5808047a66d24
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-05 00:10:34 +0900

    first commit

---
+++ Example-git5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example-git5
+//
+//  Created by 박시현 on 2022/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example-git5/Example_git5App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_git5App.swift
+//  Example-git5
+//
+//  Created by 박시현 on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct Example_git5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
